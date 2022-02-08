55a9e1fd4b97969747baf527caea399d1aa2c0ca
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2022-02-08 04:53:17 -0500

    added DataExpenseModel to better represent db objects

---
+++ KExpense.Repository/DataExpenseModel.cs
@@ -0,0 +1,24 @@
+﻿using KExpense.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KExpense.Repository
+{
+     class DataExpenseModel :  ExpenseModel
+    {
+
+        public void copy(IKExpense original)
+        {            
+            Id = original.Id;
+            SpendingOrgId = original.SpendingOrgId;
+            ExpenseDate = original.ExpenseDate;
+            BriefDescription = original.BriefDescription;
+            SpentOnName = original.SpentOnName;
+            Cost = original.Cost;
+            MerchantName = original.MerchantName;
+        }
+        public int ForProductId { get; set; } = 5;
+        public int MerchantId { get; set; } = 0;
+    }
+}
