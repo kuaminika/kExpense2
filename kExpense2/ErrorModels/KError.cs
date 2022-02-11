0eb1525a8e19bff40841f59e73f446b6f2d26be2
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2022-02-10 23:12:06 -0500

    added KError

---
+++ kExpense2/ErrorModels/KError.cs
@@ -0,0 +1,14 @@
+﻿using kExpense2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kExpense2.ErrorModels
+{
+    public class KError : IKResultModel
+    {
+        //TODO this shouldnt be hardocoded
+        public string Message => "it just didnt work";
+    }
+}
