d534284ed78c092052da1f693fd16c146a258787
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2022-02-10 23:12:51 -0500

    added IKResultModel

---
+++ kExpense2/ViewModels/SuccessModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kExpense2.ViewModels
+{
+    public struct SuccessModel: IKResultModel
+    {
+        private string msg;
+
+        public SuccessModel(string msg)
+        {
+            this.msg = msg;
+        }
+        public string Message=> $"it worked {msg}";
+    }
+}
