cd13e34bc9c508642e6e19b5cf6f16cb8758fdec
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2022-03-27 01:18:08 -0400

    added ErrorMerchantModels for error handling

---
+++ kExpense2/ErrorModels/ErrorMerhantModel.cs
@@ -0,0 +1,15 @@
+﻿using kExpense2.ViewModels;
+using MerchantService.Models;
+
+namespace kExpense2.ErrorModels
+{
+    public class ErrorMerhantModel : IMerchantModel,IKResultModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Error fetching merchant";
+        public string Phone { get; set; }
+        public string Address { get => Message; set => Message = value; } 
+        public string Message { get; set; } = "Error fetching merchant";
+    }
+
+}
