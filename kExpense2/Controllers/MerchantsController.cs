2f7eda951250dd9494eeef70800744d66a6ad555
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2022-03-27 01:30:55 -0400

    introduced MerchantsController

---
+++ kExpense2/Controllers/MerchantsController.cs
@@ -0,0 +1,41 @@
+﻿using kExpense2.kConfigs;
+using MerchantService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace kExpense2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MerchantsController : ControllerBase
+    {
+        private KxtensionsConfig _config;
+        private KExpenseToolBox toolBox;
+
+        public MerchantsController(IConfiguration Configuration)
+        {
+            _config = new KxtensionsConfig(Configuration);
+            toolBox = new KExpenseToolBox(_config);
+
+        }
+
+        [HttpGet]
+        public List<IMerchantModel> Get()
+        {
+            try 
+            {
+                List<IMerchantModel> result =  toolBox.merchantService.GetAll();
+                return result;
+            }
+            catch(Exception ex)
+            {
+                List<IMerchantModel> errList = new List<IMerchantModel>();
+                errList.Add(ErrorModels.ErrorMerchantModelCreator.get().CreateFromException(ex));
+                return errList;
+            }
+        }
+
+    }
+}
