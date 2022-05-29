e5649f21a6b3f7a6d3008094ce3556fb653f6200
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2022-05-28 22:07:28 -0400

    updated gitignore to not have appsettings.Development.json

---
+++ kExpense2/Controllers/IncomesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace kExpense2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncomesController:ControllerBase
+    {
+
+    }
+}
