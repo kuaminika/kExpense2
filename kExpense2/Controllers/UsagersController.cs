cd552eb9c42c662af769ae4e78f677c80d8a5ad2
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2022-08-13 10:53:34 -0400

    added UsagerController

---
+++ kExpense2/Controllers/UsagersController.cs
@@ -0,0 +1,29 @@
+﻿using kExpense2.kConfigs;
+using kExpense2.service.Usager;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace kExpense2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsagersController : ControllerBase
+    {
+        private IUsagerService service;
+             
+
+        public UsagersController(IConfiguration Configuration)
+        {
+            service = Igniter.Ignite(Configuration);
+        }
+
+        [HttpGet]
+        public List<UsagerModel> Get()
+        {
+            List<UsagerModel> result = service.Get(new UsagerModel());
+
+            return result;
+        }
+    }
+}
