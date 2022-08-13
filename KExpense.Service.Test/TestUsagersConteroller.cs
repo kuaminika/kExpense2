50911a09f7d50855c07295bf783295d8fbe6c60b
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2022-08-13 10:53:05 -0400

    added TestUsagersConteroller

---
+++ KExpense.Service.Test/TestUsagersConteroller.cs
@@ -0,0 +1,33 @@
+﻿using kExpense2.Controllers;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace KExpense.Service.Test
+{
+    public class TestUsagersConteroller
+    {
+        UsagersController specimen;
+        [SetUp]
+        public void SetUp()
+        {
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("kExpenseConfig.json", optional: false, reloadOnChange: false).Build();
+
+            specimen = new UsagersController(config);
+        }
+
+
+
+        [Test]
+        public void TestFetchingUsagers_NoConditions()
+        {
+            List<kExpense2.service.Usager.UsagerModel> list = specimen.Get();
+            Console.Out.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(list));
+            Assert.IsNotNull(list);
+            Assert.IsTrue(list.Count > 0);
+        }
+
+
+    }
+}
