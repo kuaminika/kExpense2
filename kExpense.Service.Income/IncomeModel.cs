57c2a02bb98e9419b9d321426983068ba00beca3
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2022-08-16 10:12:34 -0400

    updated income Model

--- kExpense.Service.Income/IncomeModel.cs
+++ kExpense.Service.Income/IncomeModel.cs
@@ -13,37 +13,26 @@ namespace kExpense.Service.Income
         public DateTime IncomeDate { get => strToDate(RawDate); set { RawDate = value.ToString("yyyyMMdd"); } }
         public string BriefDescription { get; set; }
         public decimal Amount { get; set; }
-        public RecordedSource Source { get=>_source??ghostSource(); set=>_source=value; }
+        public RecordedSource Source { get { _source = _source ?? ghostSource();
+                return _source;
+            } set=>_source=value; }
         RecordedSource _source;
         private RecordedSource ghostSource()//TODO : need to make sure this ghost source works right
         {
-            var result = new RecordedSource{ Id = SourceId, Name = SourceName, Email = SourceEmail, Phone="ghost", Address="ghost"};
+            var result = new RecordedSource{ Id = 0, Name = "", Email = "", Phone="ghost", Address="ghost"};
             return result;
         }
-        public ProductModel Product { get => _product ?? ghostProduct(); set => _product = value; }
+        public ProductModel Product { get { _product = _product ?? ghostProduct();return _product; } set => _product = value; }
         ProductModel _product;
         private ProductModel ghostProduct() { return new ProductModel { Name = "ghost", Description = "ghost", OrgId = OrgId };  }
         
-        public int SourceId { get; internal set; }
+        public int SourceId { get => Source.Id; internal set => Source.Id = value; }
         public string RawDate { get; internal set; }
         public string ProductName { get=>this.InvestmentName; set=>InvestmentName=value;  }
-        public string SourceEmail {get;set;}
-        public string SourceName {get;set;}
-        public string InvestmentName { 
-            get => Product.Name;
-            set { 
-                _product = _product ?? ghostProduct();
-                _product.Name = value;
-            } } 
-        public int ProductId
-        {
-            get => Product.Id;
-            set
-            {
-                _product = _product ?? ghostProduct();
-                _product.Id = value;
-            }
-        }
+        public string SourceEmail { get => Source.Email; internal set => Source.Email = value; }
+        public string SourceName { get => Source.Name; internal set => Source.Name = value; }
+        public string InvestmentName { get => Product.Name; internal set => Product.Name = value; }
+        public int ProductId { get => Product.Id; internal set => Product.Id = value; }    
 
         private DateTime strToDate(string str)
         {
